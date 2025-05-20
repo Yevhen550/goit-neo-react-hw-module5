807f030580b6b4c91257b82e19e71f3afc08c13b
Yevhen <benatanlev@icloud.com>
2025-05-20 10:56:13 +0300

    +

--- src/App.jsx
+++ src/App.jsx
@@ -1,38 +1,5 @@
-import { useEffect, useState } from "react";
+function App() {
+  return <div>App</div>;
+}
 
-const App = () => {
-  const [user, setUser] = useState(null);
-  const [searchParams, setSearchParams] = useSearchParams();
-  const username = searchParams.get("username");
-
-  useEffect(() => {
-    // Тут виконуємо асинхронну операцію,
-    // наприклад HTTP-запит за інформацією про користувача
-    if (username === "") return;
-
-    FakeAPI.getUser(username).then(setUser);
-  }, [username]);
-
-  const updateSearchParams = (key, value) => {
-    const updatedParams = new URLSearchParams(searchParams);
-    updatedParams.set(key, value);
-    setSearchParams(updatedParams);
-  };
-
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    const form = e.currentTarget;
-    updateSearchParams("username", form.elements.username.value);
-    form.reset();
-  };
-
-  return (
-    <>
-      <form onSubmit={handleSubmit}>
-        <input type="text" name="username" />
-        <button type="submit">Search</button>
-      </form>
-      {user && <UserInfo user={user} />}
-    </>
-  );
-};
+export default App;

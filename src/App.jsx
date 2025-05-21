1eca0012dab16055d94d3b66151a54c387d374f2
Yevhen <benatanlev@icloud.com>
2025-05-21 10:17:41 +0300

    +++

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,28 @@
+import { NavLink, Route, Routes } from "react-router-dom";
+
 function App() {
-  return <div>App</div>;
+  return (
+    <div>
+      <nav className="">
+        <NavLink to="/" className="">
+          Home
+        </NavLink>
+        <NavLink to="/about" className="">
+          About
+        </NavLink>
+        <NavLink to="/products" className="">
+          Products
+        </NavLink>
+      </nav>
+
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/products" element={<Products />} />
+        <Route path="*" element={<NotFound />} />
+      </Routes>
+    </div>
+  );
 }
 
 export default App;

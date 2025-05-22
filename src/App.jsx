9814b9e91e17023f19d215c893941fa00eb31f74
Yevhen <benatanlev@icloud.com>
2025-05-22 19:31:46 +0300

    nav

--- src/App.jsx
+++ src/App.jsx
@@ -3,10 +3,12 @@ import HomePage from "./pages/HomePage/HomePage";
 import MovieDetailsPage from "./pages/MovieDetailsPage/MovieDetailsPage";
 import MoviesPage from "./pages/MoviesPage/MoviesPage";
 import NotFoundPage from "./pages/NotFoundPage/NotFoundPage";
+import Navigation from "./components/Navigation/Navigation";
 
 function App() {
   return (
     <div>
+      <Navigation />
       <Routes>
         <Route path="/" element={<HomePage />} />
         <Route path="/movies/:movieId" element={<MovieDetailsPage />} />
--- src/components/Navigation/Navigation.jsx
+++ src/components/Navigation/Navigation.jsx
@@ -1,5 +1,19 @@
+import s from "./Navigation.module.css";
+import { NavLink } from "react-router-dom";
+
+const navLinkClass = ({ isActive }) => (isActive ? s.active : "");
+
 const Navigation = () => {
-  return <div>Navigation</div>;
+  return (
+    <nav className={s.navigation}>
+      <NavLink className={navLinkClass} to="/">
+        Home
+      </NavLink>
+      <NavLink className={navLinkClass} to="/movies">
+        Movies
+      </NavLink>
+    </nav>
+  );
 };
 
 export default Navigation;

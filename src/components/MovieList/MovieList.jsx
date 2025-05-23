9419299188fd61190e1c4e2c45cc856a5194dd5c
Yevhen <benatanlev@icloud.com>
2025-05-23 18:23:07 +0300

    +

--- src/components/MovieList/MovieList.jsx
+++ src/components/MovieList/MovieList.jsx
@@ -1,5 +1,20 @@
-const MovieList = () => {
-  return <div>MovieList</div>;
+import { Link, useLocation } from "react-router-dom";
+import s from "./";
+
+const MovieList = ({ movies }) => {
+  const location = useLocation();
+
+  return (
+    <ul className={s.list}>
+      {movies.map(({ id, title }) => (
+        <li key={id}>
+          <Link to={`./movies${id}`} state={location}>
+            {title}
+          </Link>
+        </li>
+      ))}
+    </ul>
+  );
 };
 
 export default MovieList;
